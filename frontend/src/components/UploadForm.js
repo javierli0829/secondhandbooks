f54048e6b2b6ba5e3181652d3f509696b0286992
javierli0829 <a2982601@gmail.com>
2020-05-08 00:48:54 +0800

    some updates

--- frontend/src/components/UploadForm.js
+++ frontend/src/components/UploadForm.js
@@ -8,15 +8,47 @@ function closeForm() {
   document.getElementById("uploadFormMask").style.display = "none";
 }
 
+function postBook(e) {
+  e.preventDefault();
+  const bookName = document.getElementById("bookNameInput").value;
+  const category = document.getElementById("categorySelector").value;
+  const author = document.getElementById("authorInput").value;
+  const description = document.getElementById("descriptionInput").value;
+  const peopleInterested = [1];
+  fetch('http://127.0.0.1:8000/book/', {
+    method: 'POST',
+    body: JSON.stringify(
+      {
+        owner: 1,
+        name: bookName,
+        category,
+        author,
+        description,
+        peopleInterested
+      }
+    ),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+  .then((res) => res.json())
+  .catch(error => console.log(error))
+  .then(response => console.log('Success:', response));
+}
+
 const UploadForm = (props) => {
   return (
     <div id="uploadFormMask">
       <div className="uploadFormPopup" id="uploadForm">
-        <Form>
+        <Form onSubmit={postBook}>
           <h1 className="uploadFormTitle">Upload My Book</h1>
           <FormGroup className="formGroup">
+            <Label for="bookName">Book Name</Label>
+            <Input type="text" name="bookName" id="bookNameInput" placeholder="Book Name" required/>
+          </FormGroup>
+          <FormGroup className="formGroup">
             <Label for="category">Category</Label>
-            <Input type="select" name="category" id="categorySelector">
+            <Input type="select" name="category" id="categorySelector" required>
               <option>Literature & Ficton</option>
               <option>Comic Book</option>
               <option>Magazine</option>
@@ -26,23 +58,19 @@ const UploadForm = (props) => {
             </Input>
           </FormGroup>
           <FormGroup className="formGroup">
-            <Label for="bookName">Book Name</Label>
-            <Input type="text" name="bookName" id="bookNameInput" placeholder="Book Name" />
-          </FormGroup>
-          <FormGroup className="formGroup">
             <Label for="image">Image</Label>
-            <Input type="file" name="image" id="imageSelector" />
+            <Input type="file" name="image" id="imageSelector"/>
           </FormGroup>
           <FormGroup className="formGroup">
             <Label for="author">Author</Label>
-            <Input type="text" name="author" id="authorInput" placeholder="Author" />
+            <Input type="text" name="author" id="authorInput" placeholder="Author" required/>
           </FormGroup>
           <FormGroup className="formGroup">
             <Label for="description">Description</Label>
-            <Input type="textarea" name="description" id="descriptionInput" placeholder="Description" />
+            <Input type="textarea" name="description" id="descriptionInput" placeholder="Description" required/>
           </FormGroup>
           <Button className="uploadFormBtn" onClick={closeForm}>Cancel</Button>
-          <Button className="uploadFormBtn">Submit</Button>
+          <Button className="uploadFormBtn" onSubmit={postBook}>Submit</Button>
         </Form>
       </div>
     </div>
