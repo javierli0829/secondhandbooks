dee4f1aa7e24462500c1e79ccaa639e96c142d4d
javierli0829 <a2982601@gmail.com>
2020-05-07 19:34:26 +0800

    make page classes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,76 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import '../styles/Login.css';
 import { login } from '../actions/users';
 
-// function loginButtonClick(dispatch){
-//   const account = document.getElementById("accountInput").value;
-//   const password = document.getElementById("passwordInput").value;
-//   fetch('http://127.0.0.1:8000/user/?username='+account , {})
-//   .then((response) => {
-   
-//     console.log(response);
-    
-//     return response.json(); 
-//   }).then((data) => {
-//     console.log(data);
-//     if(data.length === 0){
-//       alert("No user found.");
-//     }else{
-//       if(data[0].password === password){
-//         // dispatch(login({userId: data.id, usenName: data.username}));
-//         window.location.href = "/";
-//       }else{
-//         alert("Wrong password.");
-//       }
-//     }
-//   }).catch((err) => {
-//     console.log('err:', err);
-//   });
-// }
-
-// function Login(props) {
-//   const { handleLogin } = props;
-//   props.dispatch(login({userId: 10, userName: 'ja'}));
-//   return (
-//     <div className="Login">
-//       <Form>
-//         <h1 className="loginFormTitle">Login</h1>
-//         <FormGroup className="formGroup">
-//           <Label for="account">Account</Label>
-//           <Input type="text" name="account" id="accountInput" placeholder="Account" />
-//         </FormGroup>
-//         <FormGroup className="formGroup">
-//           <Label for="password">Password</Label>
-//           <Input type="password" name="password" id="passwordInput" />
-//         </FormGroup>
-//         <Button onClick={handleLogin} className="uploadFormBtn">Login</Button>
-//       </Form>
-//     </div>
-//   );
-// }
-
-const Login = (props) => {
-  const { handleLogin } = props;
-  // props.dispatch(login({userId: 10, userName: 'ja'}));
-  return (
-    <div className="Login">
-      <Form>
-        <h1 className="loginFormTitle">Login</h1>
-        <FormGroup className="formGroup">
-          <Label for="account">Account</Label>
-          <Input type="text" name="account" id="accountInput" placeholder="Account" />
-        </FormGroup>
-        <FormGroup className="formGroup">
-          <Label for="password">Password</Label>
-          <Input type="password" name="password" id="passwordInput" />
-        </FormGroup>
-        <Button onClick={handleLogin} className="uploadFormBtn">Login</Button>
-      </Form>
-    </div>
-  );
+class Login extends Component {
+  constructor(props){
+    super(props);
+    this.handleLogin = props.handleLogin;
+  }
+  render(){
+    return (
+      <div className="Login">
+        <Form>
+          <h1 className="loginFormTitle">Login</h1>
+          <FormGroup className="formGroup">
+            <Label for="account">Account</Label>
+            <Input type="text" name="account" id="accountInput" placeholder="Account" />
+          </FormGroup>
+          <FormGroup className="formGroup">
+            <Label for="password">Password</Label>
+            <Input type="password" name="password" id="passwordInput" />
+          </FormGroup>
+          <Button onClick={this.handleLogin} className="uploadFormBtn">Login</Button>
+        </Form>
+      </div>
+    );
+  }
 }
+
+Login.propTypes = {
+  handleLogin: PropTypes.func
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
