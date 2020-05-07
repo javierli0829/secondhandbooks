ac78f9c1c25489a3ff0c053707f63750f27c5d1a
yang zhi lin <1155092151@link.cuhk.edu.hk>
2020-05-07 20:07:59 +0800

    field can be null

--- backend/books/models.py
+++ backend/books/models.py
@@ -13,11 +13,11 @@ class Book(models.Model):
     category = models.CharField("category",max_length=30, default="")
     name = models.CharField("name",max_length=100)
     author = models.CharField("author",max_length=50)
-    description = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000, null=True)
     postedTime = models.DateTimeField("postedTime",default=datetime.now)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booksOwned")
     matched = models.BooleanField("matched",default=False)
-    peopleInterested = models.ManyToManyField(User, related_name="bookInterested")
+    peopleInterested = models.ManyToManyField(User, related_name="bookInterested", null=True, blank=True)
     image = models.ImageField(upload_to="books/", null=True, blank=True, verbose_name="image")
 
     class Meta:
