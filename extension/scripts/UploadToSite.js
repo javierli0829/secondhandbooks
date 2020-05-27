d7d3f12dd358e8c4942186e9191bc3c60816a09e
yang zhi lin <1155092151@link.cuhk.edu.hk>
2020-05-27 10:44:19 +0800

    change ext

--- extension/scripts/UploadToSite.js
+++ extension/scripts/UploadToSite.js
@@ -4,6 +4,17 @@ var category = document.getElementsByClassName("actionLinkLite bookPageGenreLink
 var description = document.querySelector("#description").children[0].innerText;
 var image = document.querySelector("#coverImage").src;
 
+if (category=="Fiction" || category=="Literature")
+    category = "1";
+else if (category=="Comics")
+    category = "2";
+else if (category=="Biography" || category=="Autobiography")
+    category = "4";
+else if (category=="Textbooks" || category=="Reference")
+    category = "5"; 
+else if (category=="Food and Drink")
+    category = "6";
+
 chrome.runtime.sendMessage( 
 { 
   bookTitle: bookTitle,
